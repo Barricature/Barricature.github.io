b9360376275557f344d34d058d9cb94d75da7de0
Barricature <irisxia2016@gmail.com>
2023-06-08 22:15:07 +0800

    A little more styling

--- src/routes/home.jsx
+++ src/routes/home.jsx
@@ -8,14 +8,22 @@ export default function Home() {
             <div class='page-body'>
                 <div>
                     <br></br>
-                    <p>This page is an evolving product that grows with my coding skills</p>
-                    <p>
-                        In 
-                        <a href='https://github.com/Barricature/Barricature.github.io/tree/9e00ef03cf405017857e23f9779c9223f989aff2'>2022</a>
-                        It was written in pure html and css
-                    </p>
-                    <p>I have now updated the page to use <a href='https://github.com/Barricature/Barricature.github.io'>React</a></p>
-                    <p>It is built with babel, webpack, gh-pages and react-router</p>
+                    <p>This page is an evolving product that grows with my coding skills, it will hopefully become fully polished one day.</p>
+                    <br></br>
+                    <ul>
+                        <li>
+                            In the summer <a href='https://github.com/Barricature/Barricature.github.io/tree/9e00ef03cf405017857e23f9779c9223f989aff2'>2022</a>, I started learning Web Programming and wrote this page in pure HTML and CSS.
+                        </li>
+                        <li>
+                            I took Web Programming in Spring 2023 and therefore imporved a lot on the matter. (See Projects/Mapic)
+                        </li>
+                        <li>
+                            I decided to learn React after I realize how painful it is to write a large project without code modularity and reusability.
+                        </li>
+                        <li>
+                            This page is now updated with <a href='https://github.com/Barricature/Barricature.github.io'>React</a>. 
+                        </li>
+                    </ul>
                 </div>
             </div>
         </>
